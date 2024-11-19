2d3820d4f068631065cc85563700ae70991f970d
kalpana udara <kalpanaudara058@gmail.com>
2024-11-19 08:15:16 +0530

    Write the test if user is not existing and refatored the test also

--- app/Action/Auth/CheckUser.php
+++ app/Action/Auth/CheckUser.php
@@ -3,12 +3,20 @@
 namespace App\Action\Auth;
 
 use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class CheckUser
 {
     public function __invoke(array $validatedUserRequest): array
     {
         $user = User::where('user_name', $validatedUserRequest['user_name'])->first();
+
+        if (!$user) {
+            return [
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => Response::$statusTexts[Response::HTTP_NOT_FOUND]
+            ];
+        }
 
         return [
             'first_name' => $user->first_name,
--- tests/Feature/User/UserAuthenticatedTest.php
+++ tests/Feature/User/UserAuthenticatedTest.php
@@ -1,9 +1,24 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 
 uses(RefreshDatabase::class);
+
+function assertTest(
+    $response,
+    int $status,
+    array $jsonStructure,
+    array $expected
+): void
+{
+    $response->assertStatus($status);
+    $response->assertJsonStructure($jsonStructure);
+    $response->assertExactJson($expected);
+}
 
 it('test user can login into the system', function () {
     $demoUser = User::factory()->create();
@@ -15,17 +30,46 @@ it('test user can login into the system', function () {
 
     $response = $this->post('api/user-sign-in', $demoUserSignInCredentials);
 
-    $response->assertStatus(200);
-    $response->assertJsonStructure([
+    assertTest(
+        $response,
+        200,
+        [
         'first_name',
         'last_name',
         'user_name',
         'role',
-    ]);
-    $response->assertExactJson([
-        'first_name' => $demoUser->first_name,
-        'last_name' => $demoUser->last_name,
-        'user_name' => $demoUser->user_name,
-        'role' => $demoUser->role,
-    ]);
+        ],
+        [
+            'first_name' => $demoUser->first_name,
+            'last_name' => $demoUser->last_name,
+            'user_name' => $demoUser->user_name,
+            'role' => $demoUser->role,
+        ]
+    );
+});
+
+
+it('test return bad response if user not existing', function () {
+    User::factory()->create();
+
+    $demoUserSignInCredentials = [
+        'user_name' => 'test',
+        'user_password' => '12345678',
+    ];
+
+   $response = $this->post('api/user-sign-in', $demoUserSignInCredentials);
+
+    assertTest(
+        $response,
+        200,
+        [
+            'status',
+            'message'
+        ],
+        [
+            'status' => Response::HTTP_NOT_FOUND,
+            'message' => Response::$statusTexts[Response::HTTP_NOT_FOUND]
+        ]
+    );
+
 });
